14fe205ab260cefa505c5b812fb99c818b617ac3
Felix Orinda <forinda82@gmail.com>
2022-07-19 02:53:09 +0300

    Refactor lecturer

--- src/api/dtos/Student.dto.ts
+++ src/api/dtos/Student.dto.ts
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { IStudent } from '@comp326-schema/Student.schema';
+import { generateHudumaNumber } from '@comp326-helpers/hudumaNumber';
 import { studentValidator } from '@comp326-validators/studentValidator';
 
 export const studentDTO = (student: IStudent) => {
 	studentValidator(student);
 
-	return {
+	return Object.freeze({
 		getFirstName: () => student.firstName,
 		getLastName: () => student.lastName,
 		getDateOfBirth: () => student.dateOfBirth,
@@ -13,9 +14,9 @@ export const studentDTO = (student: IStudent) => {
 		getEmail: () => student.email,
 		getPhone: () => student.phone,
 		getNationalId: () => student.nationalId,
-		getHudumaNumber: () => student.hudumaNumber,
+		getHudumaNumber: () => student.hudumaNumber ? student.hudumaNumber : generateHudumaNumber(),
 		getPassword: () => student.password,
 		getCourse: () => student.course,
 		getCurrentSession: () => student.currentSession,
-	};
+	});
 };
--- src/api/services/lecturer.service.ts
+++ src/api/services/lecturer.service.ts
@@ -2,7 +2,7 @@
 import { ExpressError } from '@comp326-common/errors/ExpressError';
 import { ILecturer } from '@comp326-schema/Lecturer.schema';
 import LDO from '@comp326-api/dao/Lecturer.dao';
-import { LecturerDto } from '@comp326-api/dtos/Lecturer.dto';
+import { lecturerDTO } from '@comp326-api/dtos/Lecturer.dto';
 import { createStaffRegistrationNumber as reg } from '@comp326-helpers/reg-generator/regGenerator';
 import validateMongodbId from '@comp326-helpers/validators/validateMongoId';
 
@@ -15,23 +15,44 @@ class LecturerService {
 
 	createLecturer = async (lecturer: ILecturer) => {
 		const data = await reg();
-		const newLecturer = new LecturerDto({
+		const nl = lecturerDTO({
 			...lecturer,
 			password: data.password,
 			staffId: data.staffId,
-		}).toJSon();
-		const res = await this.lecturerDao.createLecturer(newLecturer);
+		});
+		const res = await this.lecturerDao.createLecturer({
+			dateOfBirth: nl.getDateOfBirth(),
+			department: nl.getDepartment(),
+			email: nl.getEmail(),
+			firstName: nl.getFirstName(),
+			lastName: nl.getLastName(),
+			password: nl.getPassword(),
+			staffId: nl.getStaffId(),
+			hudumaNumber: nl.getHudumaNumber(),
+			phone: nl.getPhone(),
+			nationalId: nl.getNationalId(),
+			units: nl.getUnits(),
+		});
 
 		return res;
 	};
 
 	updateLecturer = async (id: string, lecturer: Partial<ILecturer>) => {
 		const existingLecturer = await this.lecturerDao.findLecturerById(id);
-		const update = { ...existingLecturer!, ...lecturer };
-		const res = await this.lecturerDao.updateLecturer(
-			id,
-			new LecturerDto(update).toJSon(),
-		);
+		const ul = lecturerDTO({ ...existingLecturer!, ...lecturer });
+		const res = await this.lecturerDao.updateLecturer(id, {
+			dateOfBirth: ul.getDateOfBirth(),
+			department: ul.getDepartment(),
+			email: ul.getEmail(),
+			firstName: ul.getFirstName(),
+			lastName: ul.getLastName(),
+			password: ul.getPassword(),
+			staffId: ul.getStaffId(),
+			hudumaNumber: ul.getHudumaNumber(),
+			phone: ul.getPhone(),
+			nationalId: ul.getNationalId(),
+			units: ul.getUnits(),
+		});
 
 		return res;
 	};
